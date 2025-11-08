b1aae3e1fd9dbc25fdadb6ae5b1b10414fa36225
Jerend <rechkovdv@gmail.com>
2025-11-08 19:48:20 +0300

    commit tests

--- tests/tests.py
+++ tests/tests.py
@@ -9,13 +9,3 @@ def test_main_page():
     response = client.get("/")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
-
-def test_search_page():
-    """Страница для поиска загружена успешно"""
-    response = client.get("/search")
-    assert response.status_code == 200
-    assert "text/html" in response.headers["content-type"]
-
-def test_static_files():
-    response = client.get("/static/")
-    assert response.status_code != 500
