395dc07481237895d19e777616c49f7e4ccf18f9
Denis Rechkov <122973608+Jerend@users.noreply.github.com>
2025-11-08 20:52:15 +0300

    Update main.py

--- app/routers/main.py
+++ app/routers/main.py
@@ -5,7 +5,6 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
